ec8b5a1019807715e0a16fc6036bb91704f4ea03
AlexaNicio <alexa_3141240243@utd.edu.mx>
2025-07-03 08:13:54 -0600

    Update main.py
    c

--- Programacion_Estructurada/P2/2_proyecto_peliculas_v2/main.py
+++ Programacion_Estructurada/P2/2_proyecto_peliculas_v2/main.py
@@ -1,1 +1,49 @@
 
+'''
+Crear un proyecto que permita gestionar (administrar) peliculas
+Colocar un menú de opciones para agregar, borrar, modificara
+mostrar, buscar, limpiar una lista de peliculas
+
+notas:
+1.-Utilizar funciones y mandar llamar desde otro 
+archivo (modelo)
+2.-Utilizar listas par almacenar los nombres de peliculas
+3.- Utilizar dict para almacenar los atributos
+nombre categoria clasificacion genero idioma de peliculas
+'''
+
+import peliculas
+
+opcion=True
+
+while opcion:
+    peliculas.borrarPantalla()
+    print("\n\t\t\t .:::Gestion de peliculas::: \n\n\t "
+    "¨1.-Crear \n\n\t 2.-Borrar \n\t 3.-Mostrar \n\t 4.-Agregar caracteristicas \n\t"
+    "5.-modificar caracteristica \n\t 6.-borrar caracteristica \n\t 7.-Salir")
+    opcion=input("\n\n\t Elige una opción: ").upper()
+
+    match opcion:
+        case"1":
+            peliculas.crearPeliculas()
+            peliculas.espereTecla()
+        case"2":
+            peliculas.borrarPeliculas()
+            peliculas.espereTecla()
+        case"3":
+            peliculas.mostrarPeliculas()
+            peliculas.espereTecla()
+        case"4":
+            peliculas.agregarCaracteristicaPeliculas()
+            peliculas.espereTecla()
+        case"5":
+            peliculas.modificarCaracteristicaPeliculas()
+            peliculas.espereTecla()
+        case"6":
+            peliculas.borrarCaracteristicaPeliculas()
+            peliculas.espereTecla()
+        case"7":
+           opcion=False
+           print("\n\t Terminaste la ejecución del sistema. Gracias")
+        case    _:
+            print("\n\t Opción invalida vuelva a intentar")
